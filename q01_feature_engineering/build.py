9d6cc1cec85303f533dbebb225c8c265638ed83e
Alex Anthony <alex19427@gmail.com>
2018-06-13 10:24:26 +0530

    Update build.py

--- q01_feature_engineering/build.py
+++ q01_feature_engineering/build.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+battles = pd.read_csv('data/battles.csv')
+character_predictions = pd.read_csv('data/character-predictions.csv')
 
 def q01_feature_engineering():
     "write your solution here"
