5798500b2bc509b3b959d418474eaacdac5b12b6
Alex Anthony <alex19427@gmail.com>
2018-06-13 11:01:00 +0530

    Update build.py

--- q04_pairs_fought_the_most_battles/build.py
+++ q04_pairs_fought_the_most_battles/build.py
@@ -3,6 +3,7 @@ from collections import Counter
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.game_of_thrones.q01_feature_engineering.build import q01_feature_engineering
+plt.switch_backend('agg') 
 
 battles = pd.read_csv('data/battles.csv')
 character_predictions = pd.read_csv('data/character-predictions.csv')
