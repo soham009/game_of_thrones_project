ac89b311004ceade8ef88ed0942713c2359aae37
Alex Anthony <alex19427@gmail.com>
2018-06-13 11:04:36 +0530

    Update build.py

--- q06_battles_on_each_region/build.py
+++ q06_battles_on_each_region/build.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 sns.set_style("white")
-
+import matplotlib.pyplot as plt
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.game_of_thrones.q01_feature_engineering.build import q01_feature_engineering
+plt.switch_backend('agg') 
 
 battles = pd.read_csv('data/battles.csv')
 character_predictions = pd.read_csv('data/character-predictions.csv')
