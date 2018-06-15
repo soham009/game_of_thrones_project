04fa8183615579b693d748d0d6ffd8217fc4c65b
Alex Anthony <alex19427@gmail.com>
2018-06-15 17:30:59 +0530

    Update build.py

--- q05_number_of_commanders/build.py
+++ q05_number_of_commanders/build.py
@@ -5,6 +5,7 @@ sns.set_style("white")
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.game_of_thrones.q01_feature_engineering.build import q01_feature_engineering
+plt.switch_backend('agg') 
 
 battles = pd.read_csv('data/battles.csv')
 character_predictions = pd.read_csv('data/character-predictions.csv')
