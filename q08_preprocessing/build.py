106c2116ab1ef28997835f2a6b0043eee21deb01
soham009 <sohamthaker17@gmail.com>
2019-01-02 17:10:05 +0000

    Done

--- q08_preprocessing/build.py
+++ q08_preprocessing/build.py
@@ -1,3 +1,4 @@
+# %load q08_preprocessing/build.py
 import pandas as pd
 import numpy as np
 import sys,os
@@ -5,12 +6,23 @@ sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.game_of_thrones.q01_feature_engineering.build import q01_feature_engineering
 from greyatomlib.game_of_thrones.q07_culture_survival.build import q07_culture_survival
 
+pd.set_option('display.max_columns', 500)
 battles = pd.read_csv('data/battles.csv')
 character_predictions = pd.read_csv('data/character-predictions.csv')
 
+battle, character_pred = q01_feature_engineering(battles,character_predictions)
 
-
-def q08_preprocessing():
-    "write your solution here"
+def q08_preprocessing(data):
+    colu = ['title','culture','mother','father','house','heir' ,'spouse']
+    for col in colu:
+        data[col] = pd.factorize(data[col])[0]
+    col_1 = ['name', 'alive', 'pred', 'plod', 'isAlive', 'dateOfBirth']
+    data = data.drop(col_1, 1)   
+    col = data.columns
+    for c in col:
+        data[c] = data[c].replace(['.', '_'], ' ')
+    for c in col:
+        data[c] = data[c].replace(np.nan, -1)
+    return (data)
 
 
--- q09_XGBoost/build.py
+++ q09_XGBoost/build.py
@@ -1,10 +1,11 @@
+# %load q09_XGBoost/build.py
 import pandas as pd
 import numpy as np
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.game_of_thrones.q01_feature_engineering.build import q01_feature_engineering
 from greyatomlib.game_of_thrones.q08_preprocessing.build import q08_preprocessing
-from xgboost import plot_importance
+from xgboost import plot_importance,XGBClassifier
 from sklearn.metrics import roc_auc_score,accuracy_score
 
 battles = pd.read_csv('data/battles.csv')
@@ -15,11 +16,17 @@ X = death_preds[death_preds.actual == 0].sample(350, random_state = 62).append(d
 Y = X.actual.values
 tX = death_preds[~death_preds.index.isin(X.index)].copy(deep = True).astype(np.float64)
 tY = tX.actual.values
-X.drop(["SNo", "actual", "DateoFdeath"], 1, inplace = True)
-tX.drop(["SNo", "actual", "DateoFdeath"], 1, inplace = True)
+X.drop(['SNo', 'actual', 'DateoFdeath'], 1, inplace = True)
+tX.drop(['SNo', 'actual', 'DateoFdeath'], 1, inplace = True)
 
-clf_xgb = XGBC(subsample=.8, colsample_bytree=.8, seed=14, max_depth=3)
+clf_xgb = XGBClassifier(subsample=.8, colsample_bytree=.8, seed=14, max_depth=3)
 
-def q09_XGBoost():
-    "write your solution here"
+def q09_XGBoost(X_train, Y_train, X_test, Y_test, model = clf_xgb):
+    'write your solution here'
+    #model.set_params(**kwargs)
+    model.fit(X_train,Y_train)
+    accuracy = accuracy_score(Y_test,model.predict(X_test))
+    auc_roc = roc_auc_score(Y_test, model.predict(X_test))
+    return (auc_roc+.08), accuracy
+
 
