1f256d878879804788da94f508f5ca3c3c284499
soham009 <sohamthaker17@gmail.com>
2019-01-02 06:25:25 +0000

    Done

--- q06_battles_on_each_region/build.py
+++ q06_battles_on_each_region/build.py
@@ -1,7 +1,8 @@
+# %load q06_battles_on_each_region/build.py
 import pandas as pd
 import numpy as np
 import seaborn as sns
-sns.set_style("white")
+sns.set_style('white')
 import matplotlib.pyplot as plt
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
@@ -11,6 +12,17 @@ plt.switch_backend('agg')
 battles = pd.read_csv('data/battles.csv')
 character_predictions = pd.read_csv('data/character-predictions.csv')
 
-def q06_battles_on_each_region():
-    "write your solution here"
+data,character_predictions1 = q01_feature_engineering(battles,character_predictions)
+    
+def q06_battles_on_each_region(data):
+    df  = data.groupby('region').sum()[['major_death', 'major_capture']]
+    df1 = data.region.value_counts()
+    df1 = df1.to_frame()
+    df1 = df1.sort_index(ascending = False)
+    df1.columns = ['No.of battles']
+    df2 = pd.concat([df,df1],axis=1)
+    c = df2.plot.bar()
+    c.set(xlabel='region',ylabel='No.of events')
+    return df
+
 
